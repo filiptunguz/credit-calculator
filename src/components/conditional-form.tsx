54438e79ec415214f239a9a46c7c5d9f46cf0151
filip.tunguz <filipkralh11@gmail.com>
2025-07-09 14:57:17 +0200

    conditional form without condition handling

--- src/components/conditional-form.tsx
+++ src/components/conditional-form.tsx
@@ -1,10 +1,24 @@
 import { Card } from '@/components/ui/card.tsx';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs.tsx';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs.tsx';
+import {
+	Select,
+	SelectTrigger,
+	SelectContent,
+	SelectItem,
+	SelectValue,
+} from '@/components/ui/select.tsx';
+import { EMPLOYMENT_TYPE_OPTIONS_MAP, type EmploymentType } from '@/lib/credit-helper.ts';
+import { useState } from 'react';
+import { Label } from '@/components/ui/label.tsx';
+import { Checkbox } from '@/components/ui/checkbox.tsx';
 
 export default function ConditionalForm() {
+	const [employmentType, setEmploymentType] = useState<EmploymentType | undefined>();
+	const [moreThanSixMonths, setMoreThanSixMonths] = useState<boolean>(false);
+
 	return (
 		<Card className="mx-auto w-[510px] max-w-full">
-			<Tabs defaultValue="mortgage w-full">
+			<Tabs defaultValue="mortgage">
 				<TabsList className="rounded-b-none px-1.5 pb-1.5 -mx-6 -mt-6 w-[calc(100%_+_3rem)]">
 					<TabsTrigger value="mortgage" className="flex-1 uppercase rounded-bl-none">
 						Mortgage
@@ -13,12 +27,38 @@ export default function ConditionalForm() {
 						Cash
 					</TabsTrigger>
 				</TabsList>
-				<TabsContent value="mortgage">
-					<>mortgage</>
-				</TabsContent>
-				<TabsContent value="cash">
-					<>cash</>
-				</TabsContent>
+				<div className="flex gap-2 w-full">
+					<Label htmlFor="employmentType">What is your employment status? </Label>
+					<Select
+						value={employmentType}
+						onValueChange={(value) => setEmploymentType(value as EmploymentType)}
+					>
+						<SelectTrigger id="employmentType" className="flex-1">
+							<SelectValue placeholder="Choose employment status" />
+						</SelectTrigger>
+						<SelectContent>
+							{Array.from(EMPLOYMENT_TYPE_OPTIONS_MAP.entries()).map(([type, label]) => (
+								<SelectItem key={type} value={type}>
+									{label}
+								</SelectItem>
+							))}
+						</SelectContent>
+					</Select>
+				</div>
+				<Label className="hover:bg-secondary/10 cursor-pointer flex items-start gap-3 rounded-lg border p-3 has-[[aria-checked=true]]:border-accent has-[[aria-checked=true]]:bg-accent/10">
+					<Checkbox
+						id="toggle-2"
+						checked={moreThanSixMonths}
+						onCheckedChange={(checked) => setMoreThanSixMonths(checked as boolean)}
+						className="data-[state=checked]:border-accent data-[state=checked]:bg-accent data-[state=checked]:text-accent-foreground"
+					/>
+					<div className="grid gap-1.5 font-normal">
+						<p className="text-sm leading-none font-medium">Employed for over 6 months</p>
+						<p className="text-muted-foreground text-sm">
+							Have you been employed for more than 6 months in the same workplace?
+						</p>
+					</div>
+				</Label>
 			</Tabs>
 		</Card>
 	);
---
+++ src/lib/credit-helper.ts
@@ -0,0 +1,44 @@
+type CreditCapacityFields = {
+	employmentType: EmploymentType;
+	moreThanSixMonths: boolean;
+};
+
+export type EmploymentType =
+	| 'contract-unlimited'
+	| 'contract-limited'
+	| 'entrepreneur'
+	| 'unemployed'
+	| 'other';
+
+export const EMPLOYMENT_TYPE_OPTIONS_MAP = new Map<EmploymentType, string>()
+	.set('contract-unlimited', 'Permanent contract')
+	.set('contract-limited', 'Fixed-term contract')
+	.set('entrepreneur', 'Entrepreneur')
+	.set('unemployed', 'Unemployed')
+	.set('other', 'Other');
+
+export const isCreditCapable = (formData: CreditCapacityFields) => {
+	const { employmentType, moreThanSixMonths } = formData;
+	return employmentType === 'contract-unlimited' && moreThanSixMonths;
+};
+
+export const creditCapableText = (formData: CreditCapacityFields) => {
+	const { employmentType, moreThanSixMonths } = formData;
+
+	switch (employmentType) {
+		case 'contract-unlimited':
+			if (moreThanSixMonths) {
+				return 'You meet the initial conditions for obtaining a loan. Continue with the form to find out the amount of credit you can get!';
+			} else {
+				return 'Most banks require information about the six-month average salary with your last employer.';
+			}
+		case 'contract-limited':
+			return 'To be eligible for a loan, you need to have a permanent contract with your employer.';
+		case 'entrepreneur':
+			return 'Loans for entrepreneurs differ from loans for individuals, so we advise you to contact your bank to find out the specific conditions for obtaining a loan.';
+		case 'unemployed':
+			return 'You need to have been employed for a consecutive 6 months (in some banks 3 months) in order to qualify for a loan.';
+		case 'other':
+			return 'If you do not fall into any of the listed categories, we advise you to contact your bank to find out the conditions for obtaining a loan.';
+	}
+};
