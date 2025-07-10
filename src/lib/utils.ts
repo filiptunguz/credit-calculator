df62e8a213f7ce828036b94fd9089964895009de
filip.tunguz <filipkralh11@gmail.com>
2025-07-10 08:44:07 +0200

    number formating util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+/**
+ * Adds commas as thousands separators to a given number.
+ */
+export function numberWithCommas(number: number) {
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
